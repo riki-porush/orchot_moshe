ceaf51817588c8275ca6cb305758c3283fd99d14
rikiPorush <r8488r@gmail.com>
2024-03-12 20:38:23 +0200

    wo

--- src/donationDetails.js
+++ src/donationDetails.js
@@ -27,7 +27,7 @@ function DonationDetails() {
     
     try {
       const response = await axios.post(
-        "https://orchot-moshe.com/.netlify/functions/server",
+        "https://orchot-moshe.com/.netlify/functions/server/write-to-json",
         { id: idNumber, donation: amount },
         {
           headers: {

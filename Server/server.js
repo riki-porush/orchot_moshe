4e6ee31ea0246d7608e1f8c261417f8cb2b73a84
rikiPorush <r8488r@gmail.com>
2024-03-12 17:28:11 +0200

    wo

--- Server/server.js
+++ Server/server.js
@@ -79,7 +79,7 @@ app.use(express.json());
 
 // Middleware לניהול CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://orchot-moshe.com'); // שינוי כתובת המקור לכתובת הנכונה
+  res.header('Access-Control-Allow-Origin', '*'); // שינוי כתובת המקור לכתובת הנכונה
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
--- src/donationDetails.js
+++ src/donationDetails.js
@@ -27,7 +27,7 @@ function DonationDetails() {
     
     try {
       const response = await axios.post(
-        "https://orchot-moshe.netlify.app/.netlify/functions/server/write-to-json",
+        "https://orchot-moshe.netlify.app/.netlify/functions/server/",
         { id: idNumber, donation: amount },
         {
           headers: {

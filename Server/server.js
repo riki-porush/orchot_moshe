4a94b7f1e07ae4c8369672743e8526fe9bdbea64
rikiPorush <r8488r@gmail.com>
2024-03-12 16:38:11 +0200

    w

--- Server/server.js
+++ Server/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 // Middleware לניהול CORS
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // נכון לכתיבת הקוד המופיע בשאלה שלך
+  res.header('Access-Control-Allow-Origin', 'https://orchot-moshe.netlify.app/.netlify/functions/server/write-to-json'); // נכון לכתיבת הקוד המופיע בשאלה שלך
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');

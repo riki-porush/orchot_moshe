7fe3a740b7db149479eacea0cc2c581e424265b5
rikiPorush <r8488r@gmail.com>
2024-03-12 20:54:10 +0200

    wo

--- Server/server.js
+++ Server/server.js
@@ -22,44 +22,45 @@ app.use((req, res, next) => {
 app.use(express.static(path.join(__dirname, "client", "build")));
 
 app.post("/write-to-json", (req, res) => {
-  const data = req.body;
-  const jsonData = JSON.stringify(data);
+  // const data = req.body;
+  // const jsonData = JSON.stringify(data);
 
-  // כתיבת הנתונים לקובץ JSON בתיקיה 'src'
-  const filePath = path.join(__dirname, "..", "src", "data.json");
+  // // כתיבת הנתונים לקובץ JSON בתיקיה 'src'
+  // const filePath = path.join(__dirname, "..", "src", "data.json");
 
-  fs.readFile(filePath, (err, fileData) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send("שגיאה בקריאת הנתונים");
-      return;
-    }
+  // fs.readFile(filePath, (err, fileData) => {
+  //   if (err) {
+  //     console.error(err);
+  //     res.status(500).send("שגיאה בקריאת הנתונים");
+  //     return;
+  //   }
 
-    let parsedData = JSON.parse(fileData);
-    console.log(parsedData);
+  //   let parsedData = JSON.parse(fileData);
+  //   console.log(parsedData);
 
-    // עדכון הרשומה הספציפית בקובץ JSON
-    parsedData = parsedData.map((item) => {
-      if (item.id === data.id) {
-        return {
-          ...item,
-          donation: Number(item.donation) + Number(data.donation),
-        };
-      } else {
-        return item;
-      }
-    });
+  //   // עדכון הרשומה הספציפית בקובץ JSON
+  //   parsedData = parsedData.map((item) => {
+  //     if (item.id === data.id) {
+  //       return {
+  //         ...item,
+  //         donation: Number(item.donation) + Number(data.donation),
+  //       };
+  //     } else {
+  //       return item;
+  //     }
+  //   });
 
-    // שמירת הנתונים לקובץ JSON
-    fs.writeFile(filePath, JSON.stringify(parsedData), (err) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send("שגיאה בשמירת הנתונים");
-        return;
-      }
-      res.status(200).send("הנתונים נשמרו בהצלחה");
-    });
-  });
+  //   // שמירת הנתונים לקובץ JSON
+  //   fs.writeFile(filePath, JSON.stringify(parsedData), (err) => {
+  //     if (err) {
+  //       console.error(err);
+  //       res.status(500).send("שגיאה בשמירת הנתונים");
+  //       return;
+  //     }
+  //     res.status(200).send("הנתונים נשמרו בהצלחה");
+  //   });
+  // });
+  res.send("hello");
 });
 
 // הגדרת נתיב לקובץ ה-HTML הראשי של ה-React app
